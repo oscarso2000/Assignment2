0897155816f0df81d89e869be7cb132309ae8cf3
k <os2304@columbia.edu>
2016-06-30 15:30:34 -0400

    Random Number Generator

--- randnum.c
+++ randnum.c
@@ -4,15 +4,22 @@
 
 int main(){
 	srand(time(NULL));
-	int r = rand()%11;
+	int r = rand()%10+1;
 	printf("A random number has been generated from 1 to 10, please type a number to guess \n");
 	int x;
-	scanf("%d" , &x);
-	if (x==r){
-		printf("You guessed correct!");
-	}else if (x>r){
-		printf("Your number is too big");
-	}	else if (x<r){
-		printf("Your number is too small");
-	}
+	do{
+		if(scanf("%d",&x)==0){
+			printf("Not a suitable character\n");
+			while(getchar() != '\n');
+		}
+		else if (x>r){
+			printf("Your number is too big \n");
+		}
+		else if(x<r||x!=0){
+			printf("Your number is too small \n");
+		}
+		else if (x==r){
+			printf("You guessed correct! \n");
+		}
+	}while(x!=r);
 }
